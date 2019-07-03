9f27c9e5132fc24985a063df9375257cdbc0b085
Igor Martins <m.oliverunb@gmail.com>
2019-07-03 07:53:29 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
+
+
+
+import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import socket from "socket.io-client"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+
+  async componentDidMount() {
+    this.subscribeToNewFiles();
+  }
+
+  subscribeToNewFiles = () => {
+    const io = socket("https://localhost:5001");
+
+    //No on do socket vc coloca o nome do evento que vc envio do node
+    io.on("platform_origin",console.log('Nova plataforma de origem') 
+    );
+  };
+
+
+  render() {
+    return(<div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>);
+  }
 }
 
-export default App;
+
+
+
